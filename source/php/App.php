1cdc24bc947bbb8dd66987f43c7f0726c5e3c961
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2026-02-19 15:41:50 +0100

    Initial commit

---
+++ source/php/App.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace {{BPREPLACENAMESPACE}};
+
+use WpService\Contracts\AddAction;
+use WpService\Contracts\AddFilter;
+use WpService\Contracts\WpEnqueueScript;
+use WpService\Contracts\WpEnqueueStyle;
+use WpService\Contracts\WpRegisterScript;
+use WpService\Contracts\WpRegisterStyle;
+use AcfService\AcfService;
+use WpUtilService\Features\Enqueue\EnqueueManager;
+
+class App
+{
+    public function __construct(
+        private EnqueueManager $wpEnqueue,
+        private AddFilter&AddAction&WpRegisterStyle&WpEnqueueStyle&WpRegisterScript&WpEnqueueScript $wpService,
+        private AcfService $acfService)
+    {
+        $this->wpService->AddAction('admin_enqueue_scripts', [$this, 'enqueueStyles']);
+        $this->wpService->AddAction('admin_enqueue_scripts', [$this, 'enqueueScripts']);
+    }
+
+    /**
+     * Enqueue required style
+     * @return void
+     */
+    public function enqueueStyles()
+    {
+        $this->wpEnqueue->add('css/{{BPREPLACESLUG}}.css');
+    }
+
+    /**
+     * Enqueue required scripts
+     * @return void
+     */
+    public function enqueueScripts()
+    {
+        $this->wpEnqueue->add('js/{{BPREPLACESLUG}}.js');
+    }
+}
